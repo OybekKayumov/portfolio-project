e9e0537281b05c22ab2f13fd8038c2f9ec8a9c48
OybekKayumov <oybek.kayumov@gmail.com>
2021-12-13 17:34:02 +0500

    updated .js for linters error

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,18 @@
-let menuOpenButton = document.querySelector("#menu-open-icon");
-let menuCloseButton = document.querySelector("#menu-close-icon");
-let mobileMenuList = document.querySelector("#mobile-menu");
-let documentBody = document.querySelector("body");
+const menuOpenButton = document.querySelector('#menu-open-icon');
+const menuCloseButton = document.querySelector('#menu-close-icon');
+const mobileMenuList = document.querySelector('#mobile-menu');
+const documentBody = document.querySelector('body');
 
-menuOpenButton.addEventListener("click", () => {
-  console.log("Display: ", mobileMenuList.style.display);
-  mobileMenuList.style.display = "block";
-  documentBody.style.overflow = "hidden";
+// mobileMenuList.style.display = 'none';
+
+menuOpenButton.addEventListener('click', () => {
+  // console.log('Display: ', mobileMenuList.style.display);
+  mobileMenuList.style.display = 'block';
+  documentBody.style.overflow = 'hidden';
 });
 
-menuCloseButton.addEventListener("click", () => {
-  console.log("Display: ", mobileMenuList.style.display);
-  mobileMenuList.style.display = "none";
-  documentBody.style.overflow = "";
+menuCloseButton.addEventListener('click', () => {
+  // console.log('Display: ', mobileMenuList.style.display);
+  mobileMenuList.style.display = 'none';
+  documentBody.style.overflow = '';
 });
