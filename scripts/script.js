75587fd51664963fdc9bf637029fe795265e0c53
OybekKayumov <oybek.kayumov@gmail.com>
2021-12-14 10:22:57 +0500

    updated .js

--- scripts/script.js
+++ scripts/script.js
@@ -3,25 +3,21 @@ const menuCloseButton = document.querySelector('#menu-close-icon');
 const mobileMenuList = document.querySelector('#mobile-menu');
 const documentBody = document.querySelector('body');
 
-// mobileMenuList.style.display = 'none';
-
 menuOpenButton.addEventListener('click', () => {
-  // console.log('Display: ', mobileMenuList.style.display);
   mobileMenuList.style.display = 'block';
   documentBody.style.overflow = 'hidden';
 });
 
 menuCloseButton.addEventListener('click', () => {
-  // console.log('Display: ', mobileMenuList.style.display);
   mobileMenuList.style.display = 'none';
   documentBody.style.overflow = '';
 });
 
 const menuItems = document.querySelectorAll('.option');
 
-for (const menuItem of menuItems) {
+menuItems.forEach((menuItem) => {
   menuItem.addEventListener('click', () => {
     mobileMenuList.style.display = 'none';
     documentBody.style.overflow = '';
   });
-}
+});
