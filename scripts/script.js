9dd97edd48fdccf978bc671e359cb16ef6bd6786
gtekle <teklegy@gmail.com>
2021-12-14 18:32:13 +0300

    Add click event handler for see project buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,24 @@
 // mobile menu
-const menuOpenButton = document.querySelector("#menu-open-icon");
-const menuCloseButton = document.querySelector("#menu-close-icon");
-const mobileMenuList = document.querySelector("#mobile-menu");
-const documentBody = document.querySelector("body");
-const menuItems = document.querySelectorAll(".option");
+const menuOpenButton = document.querySelector('#menu-open-icon');
+const menuCloseButton = document.querySelector('#menu-close-icon');
+const mobileMenuList = document.querySelector('#mobile-menu');
+const documentBody = document.querySelector('body');
+const menuItems = document.querySelectorAll('.option');
 
-menuOpenButton.addEventListener("click", () => {
-  mobileMenuList.style.display = "block";
-  documentBody.style.overflow = "hidden";
+menuOpenButton.addEventListener('click', () => {
+  mobileMenuList.style.display = 'block';
+  documentBody.style.overflow = 'hidden';
 });
 
-menuCloseButton.addEventListener("click", () => {
-  mobileMenuList.style.display = "none";
-  documentBody.style.overflow = "";
+menuCloseButton.addEventListener('click', () => {
+  mobileMenuList.style.display = 'none';
+  documentBody.style.overflow = '';
 });
 
 menuItems.forEach((menuItem) => {
-  menuItem.addEventListener("click", () => {
-    mobileMenuList.style.display = "none";
-    documentBody.style.overflow = "";
+  menuItem.addEventListener('click', () => {
+    mobileMenuList.style.display = 'none';
+    documentBody.style.overflow = '';
   });
 });
 // mobile menu end
@@ -26,43 +26,65 @@ menuItems.forEach((menuItem) => {
 //popup menu
 const works = [
   {
-    projectTitle: "Tonic",
-    projectClient: "canopy",
-    projectRole: "Back End Dev",
-    projectYear: "2015",
-    projectSnapshot: "./images/works1.png",
+    projectTitle: 'Tonic',
+    projectClient: 'canopy',
+    projectRole: 'Back End Dev',
+    projectYear: '2015',
+    projectSnapshot: './images/works1.png',
     projectDescription:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    projectTechnologies: ["html", "css", "javascript"],
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    projectTechnologies: ['html', 'css', 'javascript'],
   },
   {
-    projectTitle: "Multi-post stories",
-    projectClient: "canopy2",
-    projectRole: "Back End Dev",
-    projectYear: "2015",
-    projectSnapshot: "./images/works2.png",
+    projectTitle: 'Multi-post stories',
+    projectClient: 'canopy2',
+    projectRole: 'Back End Dev',
+    projectYear: '2015',
+    projectSnapshot: './images/works2.png',
     projectDescription:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    projectTechnologies: ["html", "css", "javascript"],
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    projectTechnologies: ['html', 'css', 'javascript'],
   },
   {
-    projectTitle: "Tonic",
-    projectClient: "canopy3",
-    projectRole: "Back End Dev",
-    projectYear: "2015",
-    projectSnapshot: "./images/works3.png",
+    projectTitle: 'Tonic',
+    projectClient: 'canopy3',
+    projectRole: 'Back End Dev',
+    projectYear: '2015',
+    projectSnapshot: './images/works3.png',
     projectDescription:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    projectTechnologies: ["html", "css", "javascript"],
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    projectTechnologies: ['html', 'css', 'javascript'],
   },
   {
-    projectTitle: "Multi-post stories",
-    projectClient: "canopy4",
-    projectRole: "Back End Dev",
-    projectYear: "2015",
-    projectSnapshot: "./images/works4.png",
+    projectTitle: 'Multi-post stories',
+    projectClient: 'canopy4',
+    projectRole: 'Back End Dev',
+    projectYear: '2015',
+    projectSnapshot: './images/works4.png',
     projectDescription:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    projectTechnologies: ["html", "css", "javascript"],
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    projectTechnologies: ['html', 'css', 'javascript'],
   },
 ];
+const seeProjectBtns = document.querySelectorAll('.see-project');
+const modalCover = document.querySelector('.popup-cover');
+const modalCloseButton = document.querySelector('.close-modal');
+
+const projectTitle = document.querySelector('#project-title');
+
+seeProjectBtns.forEach((seeProjectBtn) => {
+  seeProjectBtn.addEventListener('click', () => {
+    let projectId = seeProjectBtn.id;
+    let projectIndex =
+      parseInt(projectId.substring(projectId.length - 1), 10) - 1;
+
+    projectTitle.innerHTML = works[projectIndex].projectTitle;
+    modalCover.style.display = 'block';
+    documentBody.style.overflow = 'hidden';
+  });
+});
+
+modalCloseButton.addEventListener('click', () => {
+  modalCover.style.display = 'none';
+  documentBody.style.overflow = '';
+});
