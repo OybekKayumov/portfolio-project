27966ff726431046354f029d5563e99acee11a13
OybekKayumov <oybek.kayumov@gmail.com>
2021-12-14 20:51:16 +0500

    add dynamic content to modal

--- scripts/script.js
+++ scripts/script.js
@@ -39,7 +39,7 @@ const works = [
     projectTitle: 'Multi-post stories',
     projectClient: 'canopy2',
     projectRole: 'Back End Dev',
-    projectYear: '2015',
+    projectYear: '2016',
     projectSnapshot: './images/works2.png',
     projectDescription:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
@@ -49,7 +49,7 @@ const works = [
     projectTitle: 'Tonic',
     projectClient: 'canopy3',
     projectRole: 'Back End Dev',
-    projectYear: '2015',
+    projectYear: '2017',
     projectSnapshot: './images/works3.png',
     projectDescription:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
@@ -59,7 +59,7 @@ const works = [
     projectTitle: 'Multi-post stories',
     projectClient: 'canopy4',
     projectRole: 'Back End Dev',
-    projectYear: '2015',
+    projectYear: '2018',
     projectSnapshot: './images/works4.png',
     projectDescription:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
@@ -71,6 +71,11 @@ const modalCover = document.querySelector('.popup-cover');
 const modalCloseButton = document.querySelector('.close-modal');
 
 const projectTitle = document.querySelector('#project-title');
+const projectClient = document.querySelector('#project-client');
+const projectRole = document.querySelector('#project-role');
+const projectYear = document.querySelector('#project-year');
+const projectSnapshot = document.querySelector('#project-snapshot');
+const projectDescription = document.querySelector('#project-description');
 
 seeProjectBtns.forEach((seeProjectBtn) => {
   seeProjectBtn.addEventListener('click', () => {
@@ -79,6 +84,12 @@ seeProjectBtns.forEach((seeProjectBtn) => {
       parseInt(projectId.substring(projectId.length - 1), 10) - 1;
 
     projectTitle.innerHTML = works[projectIndex].projectTitle;
+    projectClient.innerHTML = works[projectIndex].projectClient;
+    projectRole.innerHTML = works[projectIndex].projectRole;
+    projectYear.innerHTML = works[projectIndex].projectYear;
+    projectSnapshot.src = works[projectIndex].projectSnapshot;
+    projectDescription.innerHTML = works[projectIndex].projectDescription;
+
     modalCover.style.display = 'block';
     documentBody.style.overflow = 'hidden';
   });
